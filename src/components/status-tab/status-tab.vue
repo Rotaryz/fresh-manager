51155861c04add4c59162f2624e02541406627d3
zhuwenping <zhuwenping@jkweixin.net>
2018-12-13 10:19:51 +0800

    订单列表

---
+++ src/components/status-tab/status-tab.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="status-tab">
+    <div v-for="(item, index) in tabStatus" :key="index" :style="{'margin-right': marginRight + 'px'}" :class="{'status-item-active': tabIndex === index}" class="status-item hand">
+      <div class="status-txt" @click="_setTab(index, item)">{{item.text}}</div>
+    </div>
+    <span class="line" :style="{'left': left + 'px'}"></span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'StatusTab',
+    props: {
+      tabStatus: {
+        type: Array,
+        default: () => []
+      },
+      infoTabIndex: {
+        type: Number,
+        default: 0
+      },
+      marginRight: {
+        type: Number,
+        default: 60
+      }
+    },
+    data() {
+      return {
+        tabIndex: this.infoTabIndex,
+        left: 0,
+        tabArr: null
+      }
+    },
+    watch: {
+      infoTabIndex(newVal) {
+        this.tabIndex = newVal
+      }
+    },
+    created() {
+      console.log(this.$props.tabStatus)
+    },
+    mounted() {
+      this.tabArr = document.querySelectorAll('.status-item')
+      this.left = (this.tabArr[this.tabIndex].offsetWidth - 34) / 2
+    },
+    methods: {
+      _setTab(index, item) {
+        this.tabIndex = index
+        let left = 0
+        this.tabArr.forEach((item, idx) => {
+          if (index > idx) {
+            left += item.offsetWidth + this.marginRight
+          } else if (index === idx) {
+            left += (item.offsetWidth - 34) / 2
+          }
+        })
+        this.left = left
+        this.$emit('getStatusTab', item, index)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~@design"
+  .hand
+    cursor: pointer
+  .status-tab
+    margin-left: 11px
+    display: flex
+    font-size: $font-size-16
+    line-height: 16px
+    font-family: $font-family-regular
+    color: $color-text-main
+    position: relative
+    border-bottom: 1px solid #E1E1E1
+    .status-item
+      height: 100%
+      padding-top: 30px
+      padding-bottom: 16px
+      margin-right: 60px
+      box-sizing: border-box
+      transition: all 0.3s
+      .status-txt
+        height: 100%
+    .status-item-active
+      color: $color-text-main
+      font-family: $font-family-medium
+    .line
+      transition: all .3s
+      background: $color-positive
+      height: 2px
+      width: 34px
+      position: absolute
+      left: 0
+      bottom: 0
+
+
+</style>
--- src/pages/order-list/order-list.vue
+++ src/pages/order-list/order-list.vue
@@ -1,20 +1,44 @@
 <template>
   <div class="order-list">
-    order-list
+    <div class="tab-list">
+      <status-tab :tabStatus="tabStatus" @getStatusTab="checkTab"></status-tab>
+    </div>
+    <div class="search">
+      <base-drop-down></base-drop-down>
+      <base-search :placeHolder="placeHolder"></base-search>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import StatusTab from '@components/status-tab/status-tab'
+  import BaseSearch from '@components/_base-search/_base-search'
+  import BaseDropDown from '@components/_base-drop-down/_base-drop-down'
+
   const PAGE_NAME = 'ORDER_LIST'
   const TITLE = '订单列表'
+  const ORDERSTATUS = [{text: '全部', status: 0}, {text: '待付款', status: 1}, {text: '待提货', status: 2}, {text: '已完成', status: 3}, {text: '已关闭', status: 4}]
+
 
   export default {
     name: PAGE_NAME,
+    components: {
+      StatusTab,
+      BaseSearch,
+      BaseDropDown
+      // DefaultConfirm
+    },
     page: {
       title: TITLE
     },
     data() {
-      return {}
+      return {
+        tabStatus: ORDERSTATUS,
+        placeHolder: '订单号/会员名称'
+      }
+    },
+    methods: {
+      checkTab() {}
     }
   }
 </script>
@@ -24,4 +48,10 @@
 
   .order-list
     width: 100%
+    .search
+      padding: 29px 0 30px 0
+      width: 100%
+      layout(row)
+    .tab-list
+      wdith: 100%
 </style>
