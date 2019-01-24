41a039f3a59c33496ddda34a75e27272143e3c5a
Rotary <zhouxuan@jkweixin.com>
2019-01-24 14:52:27 +0800

    更新图片列表

---
+++ src/components/phone-box/phone-box.unit.js
@@ -0,0 +1,7 @@
+import PhoneBox from './phone-box'
+
+describe('@components/phone-box', () => {
+  it('exports a valid component', () => {
+    expect(PhoneBox).toBeAComponent()
+  })
+})
---
+++ src/components/phone-box/phone-box.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="phone-box">
+    phone-box
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  const COMPONENT_NAME = 'PHONE_BOX'
+
+  export default {
+    name: COMPONENT_NAME,
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~@design"
+
+  .phone-box
+    width: 100%
+</style>
