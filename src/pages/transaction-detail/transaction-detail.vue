c379282f005f373df9cfffff3aab39f55cd70663
Ben <ben@jkweixin.com>
2019-04-12 10:42:48 +0800

    优化细节

--- src/pages/transaction-detail/transaction-detail.vue
+++ src/pages/transaction-detail/transaction-detail.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="transaction-detail table">
     <div class="down-content">
-      <span class="down-tip">下单时间</span>
+      <span class="down-tip">支付时间</span>
       <div class="down-item">
-        <base-date-select :dateInfo="settleLogsDate" placeHolder="选择下单日期" @getTime="changeDate"></base-date-select>
+        <base-date-select :dateInfo="settleLogsDate" placeHolder="选择支付日期" @getTime="changeDate"></base-date-select>
       </div>
       <!--搜索-->
       <span class="down-tip">搜索</span>
@@ -29,14 +29,16 @@
         <div class="list">
           <div v-for="(item, index) in settleLogs" :key="index" class="list-content list-box">
             <div class="list-item list-text">{{item.created_at}}</div>
-            <div class="list-item list-use">
-              <router-link tag="a" :to="{path: `/home/order-list/order-detail/${item.order_id}`}" target="_blank" append class="list-operation">{{item.order_sn}}</router-link>
-            </div>
+            <div class="list-item list-text">{{item.goods_name}}</div>
+            <div class="list-item list-text">{{item.num}}</div>
             <div class="list-item list-text">{{item.total}}</div>
             <div class="list-item list-text">{{item.service_charge}}</div>
             <div class="list-item list-text">{{item.commission}}</div>
             <div class="list-item list-text">{{item.money}}</div>
             <div class="list-item list-text">{{item.updated_at}}</div>
+            <div class="list-item list-use">
+              <router-link tag="a" :to="{path: `/home/order-list/order-detail/${item.order_id}`}" target="_blank" append class="list-operation">{{item.order_sn}}</router-link>
+            </div>
             <div class="list-item list-text"><span class="list-status" :class="{'list-status-success': item.status === 1, 'list-status-warn': item.status === 0}"></span>{{item.status_str}}</div>
           </div>
         </div>
@@ -54,7 +56,7 @@
   const PAGE_NAME = 'TRANSACTION_DETAIL'
   const TITLE = '交易明细'
 
-  const LIST_TITLE = ['下单时间', '订单号', '交易金额', '服务费', '团长佣金', '入账资金', '入账时间', '状态']
+  const LIST_TITLE = ['支付时间', '商品', '数量', '交易金额', '服务费', '团长佣金', '入账资金', '入账时间', '关联订单号', '状态']
   const EXCEL_URL = '/social-shopping/api/backend/finance/settle-logs-excel'
 
   export default {
@@ -118,6 +120,12 @@
     .list-box
       .list-item
         padding-right: 14px
+        &:nth-child(1), &:nth-child(8), &:nth-child(9)
+          flex: 1.3
+        &:nth-child(2)
+          flex: 1.7
+        &:nth-child(3)
+          flex: 0.5
         &:last-child
           flex: 0.5
 </style>
