a8bc5ed29a0b11907c3c0706ceb71eed94604aa7
guojing <939316942@qq.com>
2019-04-22 15:18:29 +0800

    修改选择团长

--- src/pages/new-market/new-market.vue
+++ src/pages/new-market/new-market.vue
@@ -729,7 +729,7 @@
       },
       // 弹窗保存
       _additionGroup() {
-        this.selectGroupList = [...this.groupSelectItem]
+        this.selectGroupList = [...this.selectGroupList, ...this.groupSelectItem]
         this.groupList = this.groupList.map(item => {
           item.checked && (item.right = true)
           return item
