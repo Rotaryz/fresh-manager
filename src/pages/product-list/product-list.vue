8378ec2b49ef0b4fc78c8604abf0973f8a311431
wengtianliang <wengtianliang@jkweixin.com>
2019-04-26 18:25:30 +0800

    新建exec刷新列表

--- src/pages/product-list/product-list.vue
+++ src/pages/product-list/product-list.vue
@@ -347,6 +347,11 @@
         this.$loading.show('上传中...')
         let res = index === 1 ? await API.Product.goodsNewInto(param) : await API.Product.goodsNewEdit(param)
         this.$loading.hide()
+        if (res.error === this.$ERR_OK) {
+          this.goodsPage = 1
+          this.$refs.pagination.beginPage()
+          this.getGoodsListData()
+        }
         this.$toast.show(res.message)
         e.target.value = ''
       },
