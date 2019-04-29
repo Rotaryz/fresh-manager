c1f256831fe2ad994126b57090c1ce91c2d80ada
guojing <939316942@qq.com>
2019-04-30 01:38:50 +0800

    团长列表

--- src/pages/edit-leader/edit-leader.vue
+++ src/pages/edit-leader/edit-leader.vue
@@ -18,7 +18,11 @@
           团长账号
         </div>
         <div class="edit-input-box">
-          <input v-model="leaderData.mobile" type="number" class="edit-input" maxlength="11" :disabled="id"
+          <input v-model="leaderData.mobile"
+                 oninput="if(value.length > 11)value = value.slice(0, 11)"
+                 type="number"
+                 class="edit-input"
+                 :disabled="id"
                  @mousewheel.native.prevent
           >
         </div>
@@ -75,6 +79,60 @@
         </div>
       </div>
     </div>
+
+    <!--佣金设置-->
+    <div class="content-header">
+      <div class="content-title">佣金设置<span class="text">（佣金比例范围是5%-25%）</span></div>
+    </div>
+    <div class="activity-box">
+      <div class="activity-tab">
+        <div class="edit-title">
+          <span class="start">*</span>
+          <span>选择类型</span>
+        </div>
+        <div class="check-box hand" @click="selectCommission">
+          <span class="check" :class="{'checked': +leaderData.commission_type === 1}"></span>
+          <span class="text">固定比例佣金</span>
+        </div>
+        <div class="check-box hand" @click="selectCommission">
+          <span class="check" :class="{'checked': +leaderData.commission_type === 2}"></span>
+          <span class="text">阶梯比例佣金</span>
+        </div>
+      </div>
+      <div v-if="+leaderData.commission_type === 1" class="fixed-main">
+        <input v-model="leaderData.fixed_rate" placeholder="佣金结算以实际交易价为准" type="text" class="fixed-commission">
+        <span class="mark">%</span>
+      </div>
+      <div v-if="+leaderData.commission_type === 2" class="rate-main">
+        <p class="title">
+          <span class="left">销售金额</span>
+          <span class="right">佣金比例</span>
+        </p>
+        <transition-group>
+          <div v-for="(item, index) in ladder_rules" :key="index" class="rate-list">
+            <div class="left">
+              <input v-model="item.money"
+                     type="text"
+                     :placeholder="index === 0 ? 0 : ''"
+                     :readonly="index === 0"
+                     class="left-input"
+                     :class="{'disabled': index === 0}"
+                     @blur="numBlur(index)"
+              >
+              <img src="./icon-ride@2x.png" alt="" class="icon">
+              <span class="text">大于</span>
+            </div>
+            <div class="right">
+              <input v-model="item.rate" type="text" class="right-input" @blur="rateBlur(index)">
+              <span class="mark">%</span>
+              <img v-if="index > 1" src="./icon-trash@2x.png" class="del-icon hand" @click="delCommission(index)">
+            </div>
+          </div>
+        </transition-group>
+        <p class="add-list hand" @click="addCommission">添加阶梯佣金</p>
+      </div>
+      <!--<p class="add-list hand" @click="testData">测试数据</p>-->
+    </div>
     <div class="back">
       <div class="back-cancel back-btn hand" @click="_back">返回</div>
       <div class="back-btn back-submit" @click="submit">保存</div>
@@ -89,6 +147,7 @@
 
   const PAGE_NAME = 'EDIT_LEADER'
   const TITLE = '新建团长'
+  const NUM = /^(([1-9][0-9]*)|([1-9][0-9]*\.\d{1,2}))$/
 
   export default {
     name: PAGE_NAME,
@@ -119,9 +178,13 @@
           district: '',
           address: '',
           longitude: '',
-          latitude: ''
+          latitude: '',
+          fixed_rate: '7',
+          commission_type: 1
         },
-        isSubmit: false
+        isSubmit: false,
+        checked: true,
+        ladder_rules: [{money: '0', rate: ''}, {money: '', rate: ''}]
       }
     },
     created() {
@@ -129,11 +192,57 @@
     },
     mounted() {
       this._setData()
-      this.leaderData = _.cloneDeep(this.detail)
+      if (this.id) {
+        this.leaderData = _.cloneDeep(this.detail)
+        this.ladder_rules = this.leaderData.ladder_rules
+        if(this.ladder_rules.length < 2) {
+          this.ladder_rules = [{money: '0', rate: ''}, {money: '', rate: ''}]
+        }
+        if (+this.leaderData.commission_type === 2) {
+          this.$set(this.leaderData, 'fixed_rate', '7')
+        } else if (+this.leaderData.commission_type === 1) {
+          this.ladder_rules = [{money: '0', rate: ''}, {money: '', rate: ''}]
+        }
+      }
     },
     methods: {
       _back() {
         this.$router.back()
+      },
+      selectCommission() {
+        this.leaderData.commission_type = +this.leaderData.commission_type === 1 ? 2 : 1
+      },
+      addCommission() {
+        this.ladder_rules.push({})
+      },
+      delCommission(index) {
+        this.ladder_rules.splice(index, 1)
+      },
+      numBlur(index) {
+        if (this.ladder_rules[index].rate && index > 1) {
+          for (let i=0; i<this.ladder_rules.length; i++) {
+            for (let j=i+1; j<this.ladder_rules.length; j++) {
+              if (+this.ladder_rules[i].money > +this.ladder_rules[j].money) {
+                let obj = this.ladder_rules[i]
+                this.$set(this.ladder_rules, i, this.ladder_rules[j])
+                this.$set(this.ladder_rules, j, obj)
+              }
+            }
+          }
+        }
+      },
+      rateBlur(index) {
+        if (this.ladder_rules[index].money && index > 1) {
+          for (let i=0; i<this.ladder_rules.length; i++) {
+            for (let j=i+1; j<this.ladder_rules.length; j++) {
+              if (+this.ladder_rules[i].money > +this.ladder_rules[j].money) {
+                let obj = this.ladder_rules[i]
+                this.$set(this.ladder_rules, i, this.ladder_rules[j])
+                this.$set(this.ladder_rules, j, obj)
+              }
+            }
+          }
+        }
       },
       submit() {
         if (this.isSubmit) {
@@ -142,6 +251,8 @@
         if (!this._isDataValidate()) {
           return
         }
+        let result = this.testRateData()
+        if (!result && +this.leaderData.commission_type === 2) return
         this.isSubmit = false
         let address = this.leaderData.province + this.leaderData.city + this.leaderData.district + this.leaderData.address
         let oAjax = new XMLHttpRequest()
@@ -157,26 +268,64 @@
             let location = res.geocodes[0].location.split(',')
             this.leaderData.longitude = location[0]
             this.leaderData.latitude = location[1]
-
+            let data = Object.assign(this.leaderData, {ladder_rules: this.ladder_rules})
             if (this.id) {
               // 编辑团长
-              res = await API.Leader.editLeader(this.id, this.leaderData)
+              res = await API.Leader.editLeader(this.id, data)
             } else {
               // 新建团长
-              res = await API.Leader.storeLeader(this.leaderData)
+              res = await API.Leader.storeLeader(data)
             }
 
             this.$loading.hide()
             this.$toast.show(res.message)
             if (res.error !== this.$ERR_OK) {
               this.isSubmit = false
+              this.$toast.show(res.message)
               return
             }
+            setTimeout(() => {
+              this.isSubmit = false
+            }, 2000)
             setTimeout(() => {
               this._back()
             }, 1000)
           }
         }
+      },
+      testData() {
+        this.testRateData()
+      },
+      testRateData() {
+        let testArr = this.ladder_rules.map((item, index) => {
+          if ((index > 0 && +this.leaderData.commission_type === 2) && (!item.money || item.money < 0 || !NUM.test(item.money) || +item.money.split('.')[1] > 0)) {
+            this.$toast.show('请输入正整数销售金额')
+            return 'false'
+          } else if (+this.leaderData.commission_type === 2 && (!NUM.test(item.rate) || +item.rate.split('.')[1] > 0)) {
+            this.$toast.show('请输入5%-25%之间的佣金值')
+            return 'false'
+          } else if (+this.leaderData.commission_type === 2 && item.rate > 25) {
+            this.$toast.show('请输入5%-25%之间的佣金值')
+            return 'false'
+          } else if (+this.leaderData.commission_type === 2 && item.rate < 5) {
+            this.$toast.show('请输入5%-25%之间的佣金值')
+            return 'false'
+          }
+        })
+        let repeat = this.ladder_rules.map(item => {
+          let findArr = this.ladder_rules.filter(val => {
+            return item.money === val.money
+          })
+          if (findArr.length > 1) {
+            this.$toast.show('销售金额输入重复')
+            return 'false'
+          }
+        })
+        testArr = testArr.concat(repeat)
+        let result = testArr.every(item => {
+          return item !== 'false'
+        })
+        return result
       },
       /**
        * 设置默认数据 -> 编辑状态
@@ -212,6 +361,15 @@
           return
         } else if (!this.leaderData.address) {
           this.$toast.show('请输入正确的详情地址')
+          return
+        } else if ((!NUM.test(this.leaderData.fixed_rate) || this.leaderData.fixed_rate.split('.')[1] > 0) && +this.leaderData.commission_type === 1) {
+          this.$toast.show('请输入5%-25%之间的佣金比例')
+          return
+        } else if (+this.leaderData.fixed_rate < 5 && +this.leaderData.commission_type === 1) {
+          this.$toast.show('请输入5%-25%之间的佣金比例')
+          return
+        } else if (+this.leaderData.fixed_rate > 25 && +this.leaderData.commission_type === 1) {
+          this.$toast.show('请输入5%-25%之间的佣金比例')
           return
         }
         return true
@@ -297,4 +455,156 @@
         font-size: $font-size-14
         color: #acacac
         margin-left: 10px
+  .content-title .text
+    color: $color-text-assist
+    font-size: $font-size-14
+  .activity-box
+    margin-top: 25px
+    position: relative
+    .activity-tab
+      display: flex
+      algin-items: center
+      .edit-title
+        color: #2A2A2A
+        font-size: $font-size-14
+        font-family: $font-family-regular
+        text-align: right
+        width: 90px
+        margin-right: 40px
+        .start
+          display: inline-block
+          color: #F52424
+      .check-box
+        display: flex
+        align-items: center
+        margin-right: 50px
+      .check
+        width: 16px
+        height: 16px
+        border: 1px solid #E1E1E1
+        border-radius: 50%
+        transition: all 0.3s
+        display: flex
+        justify-content: center
+        align-items: center
+        margin-right: 10px
+      .checked
+        border: 5px solid $color-main
+      .text
+        color: $color-text-main
+        font-family: $font-family-regular
+        font-size: $font-size-14
+
+    .fixed-main
+      width: 523px
+      height: 80px
+      background: #F5F7FA
+      display: flex
+      align-items: center
+      padding-left: 28px
+      box-sizing: border-box
+      margin-left: 128px
+      margin-top: 20px
+      border: 0.5px solid #E9ECEE
+      .fixed-commission
+        width: 278px
+        height: 40px
+        padding: 0 12px
+        box-sizing: border-box
+        font-size: $font-size-14
+        font-family: $font-family-regular
+        color: $color-text-main
+        margin-right: 10px
+        border: 0.5px solid $color-line
+        &:hover
+          border: 1px solid #ACACAC
+        &::placeholder
+          font-family: $font-family-regular
+          color: $color-text-assist
+        &:focus
+          border-color: $color-main !important
+        &::-webkit-input-placeholder{
+          font-family: $font-family-regular
+          color: $color-text-assist
+        }
+    .rate-main
+      width: 564px
+      background: #F5F7FA
+      padding: 20px 28px
+      box-sizing: border-box
+      margin-left: 128px
+      margin-top: 20px
+      border: 0.5px solid #E9ECEE
+      font-family: $font-family-regular
+      font-size: $font-size-14
+      .title
+        display: flex
+        align-items: center
+        color: #999
+        font-family: $font-family-regular
+        .left
+          width: 184px
+        .right
+          width: 317px
+      .rate-list
+        display: flex
+        align-items: center
+        margin-top: 15px
+        .left
+          width: 184px
+          position: relative
+          .left-input
+            width: 163px
+            height: 40px
+            padding-left: 87px
+            padding-right: 9px
+            color: $color-text-main
+            font-family: $font-family-regular
+            border: 0.5px solid $color-line
+            box-sizing: border-box
+            &:hover
+              border: 1px solid #ACACAC
+            &::placeholder
+              font-family: $font-family-regular
+              color: $color-text-assist
+            &:focus
+              border-color: $color-main !important
+          .disabled
+            color: $color-text-assist
+          .text
+            color: $color-text-assist
+            col-center()
+            left: 20px
+          .icon
+            width: 14px
+            height: 14px
+            margin: 0 3px
+        .right
+          width: 325px
+          .right-input
+            width: 268px
+            height: 40px
+            padding: 0 9px
+            color: $color-text-main
+            font-family: $font-family-regular
+            border: 0.5px solid $color-line
+            &:hover
+              border: 1px solid #ACACAC
+            &::placeholder
+              font-family: $font-family-regular
+              color: $color-text-assist
+            &:focus
+              border-color: $color-main !important
+          .mark
+            margin-left: 10px
+            color: #333
+          .del-icon
+            width: 13px
+            height: 14px
+            margin-left: 20px
+      .add-list
+        color: #3E77C3
+        text-decoration: underline
+        margin-top: 20px
+        display: inline-block
 </style>
