c96604b48a46f316783ef49a4e91b02bdba12e58
zhouxuan <zhouxuan@jkweixin.com>
2019-05-16 14:34:43 +0800

    打印demo

--- src/pages/demo/demo.vue
+++ src/pages/demo/demo.vue
@@ -1,13 +1,46 @@
 <template>
   <div class="demo">
     demo
-    <add-goods ref="addg" @batchAddition="_batchAddition"></add-goods>
+    <!--<add-goods ref="addg" @batchAddition="_batchAddition"></add-goods>-->
     <span @click="deleteGoods()">sahNGV</span>
+    <div v-show="true" class="big">
+      <div id="printTest">
+        <div>
+          <!--<p class="name">荞麦饼干</p>-->
+          <!--<div class="msg">-->
+          <!--<div class="content">原味</div>-->
+          <!--<div class="money">售价：12：00</div>-->
+          <!--</div>-->
+          <!--<img src="./2-140412125945415.jpg" alt="" class="img">-->
+          <img v-if="true" id="barcode" class="barcodes">
+        </div>
+        <!--<div>-->
+        <!--<p class="name">巧克力饼干</p>-->
+        <!--<div class="msg">-->
+        <!--<div class="content">原味</div>-->
+        <!--<div class="money">售价：30：00</div>-->
+        <!--</div>-->
+        <!--<img :src="img" class="barcodes">-->
+        <!--</div>-->
+        <!--<p>锄禾日当午</p>-->
+        <!--<p>汗滴禾下土 </p>-->
+        <!--<p>谁知盘中餐</p>-->
+        <!--<p>粒粒皆辛苦</p>-->
+        <!--<img src="http://img.atobo.com/ProductImg/EWM/UWeb/2/3/8/1/5693/23815693/1.gif" class="img">-->
+      </div>
+    </div>
+    <button v-print="'#printTest'">打印</button>
+    <!--<section ref="print">-->
+    <!--打印内容-->
+    <!--<div class="no-print">不要打印我</div>-->
+    <!--</section>-->
+    <!--<button @click="printAll">打印</button>-->
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import AddGoods from '@components/add-goods/add-goods'
+  // import AddGoods from '@components/add-goods/add-goods'
+  import JsBarcode from 'jsbarcode'
 
   const PAGE_NAME = 'DEMO'
   const TITLE = '模板'
@@ -17,12 +50,31 @@
       title: TITLE
     },
     components: {
-      AddGoods
+      // AddGoods
     },
     data() {
-      return {}
+      return {
+        img: ''
+      }
+    },
+    mounted() {
+      // this.barcode()
     },
     methods: {
+      barcode() {
+        // JsBarcode生成条形码
+        JsBarcode('#barcode')
+          .options({font: 'OCR-B'})
+          .EAN13('6927999300031', {fontSize: 16, textMargin: 0})
+          .blank(20)
+          .render()
+        let qrImage = document.querySelector('#barcode')
+        this.img = qrImage.src
+      },
+      printAll() {
+        // let htmls = window.document.body.innerHTML
+        this.$print(this.$refs.print)
+      },
       _batchAddition(list) {
         console.log(list)
       },
@@ -38,4 +90,44 @@
 
   .demo
     width: 100%
+  @media print {
+    html, body {
+      height: inherit
+    }
+  }
+  .big
+    display: block
+  @page
+    color: #000
+    margin: 3mm 0 0
+    size: auto A4 landscape
+  #printTest
+    color: #000
+    padding: 0
+    margin: 0
+    display: flex
+    flex-direction: column
+    align-items: center
+    text-align: center
+    /*width: 342px*/
+    /*height: 205px*/
+    box-sizing: border-box
+    margin-left: -10px
+  .img
+    width: 60px
+  .name
+    font-size: 24px
+  .msg
+    margin: 8px auto
+    display: flex
+    width: 60%
+    font-size: 14px
+    justify-content: space-between
+  .barcodes
+    width: 210px
+    margin-left: 18px
+
+  .img
+    margin-top: -10px
+    width: 190px
 </style>
