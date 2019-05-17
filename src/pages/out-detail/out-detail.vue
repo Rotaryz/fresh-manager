7b3f69b3adbea019bbff3bd6b89560f6c5784177
wengtianliang <wengtianliang@jkweixin.com>
2019-05-17 10:22:14 +0800

    fix 采购端任务样式，出库为零样式

--- src/pages/out-detail/out-detail.vue
+++ src/pages/out-detail/out-detail.vue
@@ -35,15 +35,19 @@
             <div class="list-item list-item-batches hand" @mouseenter="_showTip(index)" @mouseleave="_hideTip" @click="outFn(item, index)">
               <transition name="fade">
                 <div v-show="showIndex === index && item.status !== 0 && item.out_batches.length" class="batches-box">
-                  <div v-for="(item1, index1) in item.out_batches" :key="index1" class="batches-box-item">
-                    {{item1.batch_num}}: 出库{{item1.select_out_num}}{{item.base_unit}}
+                  <div v-for="(item1, index1) in item.out_batches" :key="index1">
+                    <div v-if="item1.select_out_num * 1 > 0" class="batches-box-item">
+                      {{item1.batch_num}}: 出库{{item1.select_out_num}}{{item.base_unit}}
+                    </div>
                   </div>
                 </div>
               </transition>
               <transition name="fade">
                 <div v-show="showIndex === index && item.status === 0 && item.select_batch.length" class="batches-box">
-                  <div v-for="(item1, index1) in item.select_batch" :key="index1" class="batches-box-item">
-                    {{item1.batch_num}}: 出库{{item1.select_out_num}}{{item.base_unit}}
+                  <div v-for="(item1, index1) in item.select_batch" :key="index1">
+                    <div v-if="item1.select_out_num * 1 > 0" class="batches-box-item">
+                      {{item1.batch_num}}: 出库{{item1.select_out_num}}{{item.base_unit}}
+                    </div>
                   </div>
                 </div>
               </transition>
--- src/pages/procurement-task/procurement-task.vue
+++ src/pages/procurement-task/procurement-task.vue
@@ -166,7 +166,7 @@
           <div class="list-header list-box-supplier list-box">
             <div v-for="(item,index) in supplierList" :key="index" class="list-item">{{item}}</div>
           </div>
-          <div class="list">
+          <div class="list supplier-box-sort">
             <div v-for="(item, index) in supplierSortList" :key="index" class="list-content list-box-supplier list-box">
               <div class="list-item">{{item.supplier_name}}</div>
               <div class="list-item">{{item.task_count}}</div>
@@ -570,7 +570,15 @@
           this.$toast.show(res.message)
           return
         }
-        this.supplierSortList = res.data
+        this.supplierSortList.push(...res.data)
+        this.supplierSortList.push(...res.data)
+        this.supplierSortList.push(...res.data)
+        this.supplierSortList.push(...res.data)
+        this.supplierSortList.push(...res.data)
+        this.supplierSortList.push(...res.data)
+        this.supplierSortList.push(...res.data)
+        this.supplierSortList.push(...res.data)
+        this.supplierSortList.push(...res.data)
         this.$refs.selectSupplier.showModal()
       },
       async jumpTaskList(item) {
@@ -1070,7 +1078,7 @@
     width: 1000px
     min-height: 650px
     border-radius: 3px
-    padding: 0 20px
+    padding: 0 20px 24px
     .title-box
       display: flex
       box-sizing: border-box
@@ -1109,7 +1117,27 @@
       width: 0
       height: 0
       border-radius: 50%
+  .supplier-box-sort
+    height: 540px
+    overflow: auto
+    white-space: nowrap
+    transition: all 0.2s
 
+    &::-webkit-scrollbar
+      width: 0
+      height: 0
+      transition: all 0.2s
+
+    &::-webkit-scrollbar-thumb
+      background-color: rgba(0, 0, 0, .15)
+      border-radius: 10px
+
+    &::-webkit-scrollbar-thumb:hover
+      background-color: rgba(0, 0, 0, .3)
+
+    &::-webkit-scrollbar-track
+      box-shadow: inset 0 0 6px rgba(0, 0, 0, .15)
+      border-radius: 10px
   .select-icon-active
     border: 5px solid $color-main
 </style>
