02ff153faf7cfbb456dd735a27faeb068ebd4508
guojing <939316942@qq.com>
2019-06-06 12:37:33 +0800

    修改比率计算

--- src/pages/goods-data/goods-data.vue
+++ src/pages/goods-data/goods-data.vue
@@ -737,12 +737,12 @@
         // 销售数
         let salesNum = dataArr.map(item => {
           let num = (item.sales_num / data.sales_num * 100).toFixed(2)
-          return Number.isFinite(num)?num:0
+          return Number.isFinite(Number(num))?num:0
         })
         // 采购数
         let purchaseNum = dataArr.map(item => {
           let num = (item.purchase_num / data.purchase_num * 100).toFixed(2)
-          return Number.isFinite(num)?num:0
+          return Number.isFinite(Number(num))?num:0
         })
         let salesNumAll = data.sales_num
         let purchaseNumAll = data.purchase_num
